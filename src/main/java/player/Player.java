e8086985634948d821f99f9b80d68648c5d37503
VPlayer <yuyang.lee1991>
2016-08-30 00:44:32 +1000

    Initial commit

---
+++ src/main/java/player/Player.java
@@ -0,0 +1,10 @@
+package player;
+
+/**
+ * Created by Daniel-li121 on 8/30/2016.
+ */
+public class Player {
+    public static void main(String[] args) {
+        System.out.println("Hello, This is an environment testing greeting!!!");
+    }
+}
